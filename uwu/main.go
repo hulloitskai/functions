c405c25d23121f44b36e570c9e5af53fed6a2b08
Steven Xie <dev@stevenxie.me>
2019-04-15 01:15:34 -0400

    Made 'uwu' more realistic

--- uwu/main.go
+++ uwu/main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
+	"strconv"
 )
 
 // Handler destroys all of mankind in one clean swoop.
 func Handler(w http.ResponseWriter, r *http.Request) {
-	for i := 0; i < 69420; i++ {
+	params := r.URL.Query()
+
+	limit := 69420
+	if h := params.Get("horniness"); h != "" {
+		hi, err := strconv.Atoi(h)
+		if err != nil {
+			fmt.Fprintf(w, "An error occurred: %v", err)
+			return
+		}
+		limit = hi
+	}
+
+	for i := 0; i < limit; i++ {
 		io.WriteString(w, "UwU ")
 	}
 }
